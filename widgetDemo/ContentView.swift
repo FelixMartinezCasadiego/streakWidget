5607f3402ff0464240b8579258955452d8147507
felix <felix.martinezcasadiego@gmail.com>
2023-12-12 11:52:04 -0600

    Initial Commit

---
+++ widgetDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  widgetDemo
+//
+//  Created by Felix Martinez Casadiego on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ widgetDemo/widgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetDemoApp.swift
+//  widgetDemo
+//
+//  Created by Felix Martinez Casadiego on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct widgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
